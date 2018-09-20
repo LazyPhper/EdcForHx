5edac7975da828cc77fa781bc609bfc3409a585f
LazyPhper <1016122628@qq.com>
2018-09-20 09:53:53 +0800

    201809200953-one

---
+++ app/admin/controller/ProjectController.php
@@ -0,0 +1,79 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\admin\controller;
+
+use cmf\controller\AdminBaseController;
+use think\Db;
+
+class ProjectController extends AdminBaseController
+{
+
+
+    /**
+     * 项目
+    */
+    public function index()
+    {
+        $content = hook_one('admin_user_index_view');
+
+        if (!empty($content)) {
+            return $content;
+        }
+
+        $where = ["user_type" => 1];
+        /**搜索条件**/
+        $userLogin = $this->request->param('user_login');
+        $userEmail = trim($this->request->param('user_email'));
+
+        if ($userLogin) {
+            $where['user_login'] = ['like', "%$userLogin%"];
+        }
+
+        if ($userEmail) {
+            $where['user_email'] = ['like', "%$userEmail%"];;
+        }
+        $users = Db::name('user')
+            ->where($where)
+            ->order("id DESC")
+            ->paginate(10);
+        $users->appends(['user_login' => $userLogin, 'user_email' => $userEmail]);
+        // 获取分页显示
+        $page = $users->render();
+
+        $rolesSrc = Db::name('role')->select();
+        $roles    = [];
+        foreach ($rolesSrc as $r) {
+            $roleId           = $r['id'];
+            $roles["$roleId"] = $r;
+        }
+        $this->assign("page", $page);
+        $this->assign("roles", $roles);
+        $this->assign("users", $users);
+        return $this->fetch();
+    }
+    /*
+    *添加项目
+    */
+    public function add()
+    {
+
+        return $this->fetch();
+    }
+
+     /*
+    *生成项目crf
+    */
+    public function edcadd()
+    {
+
+        return $this->fetch();
+    }
+}
